27023cded2ff4421d9c1e6cd7068551827964bd7
htetz04 <simontun.zlh@gmail.com>
2026-09-12 19:45:28 -0400

    dummy files added

---
+++ tb_jeff.sv
@@ -0,0 +1,17 @@
+module tb_jeff();
+    logic [31:0] no, me, gusta;
+    logic clk, nRST;
+
+    always_ff@(posedge clk, negedge nRST) begin
+        if(!nRST) begin
+            no <= 32'h0;
+            me <= 32'h0;
+            gusta <= 32'h0;
+        end else begin
+            no <= no + 6;
+            me <= me + 7;
+            gusta <= gusta + 67;
+        end
+    end
+
+endmodule
