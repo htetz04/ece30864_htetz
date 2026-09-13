d314c99b46ecbbdcc42dbba088ae9f7a4d10879a
htetz04 <simontun.zlh@gmail.com>
2026-09-12 20:05:19 -0400

    CSP-19 commitE

--- tb_jeff.sv
+++ tb_jeff.sv
@@ -2,6 +2,8 @@ module tb_jeff();
     logic [31:0] no, me, gusta;
     logic clk, nRST;
 
+    logic commitE;
+
     always_ff@(posedge clk, negedge nRST) begin
         if(!nRST) begin
             no <= 32'h0;
